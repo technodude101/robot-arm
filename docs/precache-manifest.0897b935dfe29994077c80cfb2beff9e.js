0a16ced13ab5022d9b97632005ce6e1a958dd859
Jared Rolt <jrolt92@gmail.com>
2019-03-16 17:39:10 +0800

    build in docs folder

---
+++ docs/precache-manifest.0897b935dfe29994077c80cfb2beff9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bc47b091904f777204f",
+    "url": "/robot-arm/static/js/runtime~main.cb518901.js"
+  },
+  {
+    "revision": "11d5c3eac5a728a2dd09",
+    "url": "/robot-arm/static/js/main.2e365310.chunk.js"
+  },
+  {
+    "revision": "889da758bfe3c748f3ef",
+    "url": "/robot-arm/static/js/2.1a9a641d.chunk.js"
+  },
+  {
+    "revision": "11d5c3eac5a728a2dd09",
+    "url": "/robot-arm/static/css/main.ecf1daa1.chunk.css"
+  },
+  {
+    "revision": "a931a7608cd3d126d9d26884ef04877b",
+    "url": "/robot-arm/index.html"
+  }
+];
